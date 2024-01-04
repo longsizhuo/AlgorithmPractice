16ee2e79c4c7d7c8b26b945084619fc9e5958179
longsizhuo <longsizhuo@gmail.com>
2024-01-05 03:54:06 +0800

    蓝桥杯，启动！

--- loong's code/leetcode/editor/cn/[62]不同路径.py
+++ loong's code/leetcode/editor/cn/[62]不同路径.py
@@ -54,9 +54,8 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0] * n for _ in range(m)]
         dp[0][0] = 1
-        for i in range(1, m):
-            for j in range(1, n):
-                print(dp)
+        for i in range(m):
+            for j in range(n):
                 if i > 0 and j > 0:
                     dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
                 elif i > 0:
